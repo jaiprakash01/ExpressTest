486a93c6f4379d268735a88ba46c87358a11d87b
jaiprakash <jaybhatisingh@gmail.com>
2018-08-22 08:45:10 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const hbs = require('hbs');
+
+var app = express();
+
+app.set('view engine' , 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partialViews');
+
+//app.get('/' , (req,res)=> {
+//   res.send('<h1>Hello from Express Js</h1>');
+//});
+
+app.get('/' , (req,res)=> {
+   //res.send({
+  //   name : 'vikash',
+  //   emailids : ['vikash@gmail.com' , 'suresh@gamil.com']
+   //});
+   res.render('home.hbs', {
+     currentYear :  new Date().getFullYear()
+   });
+});
+
+app.get('/About' , (req, res) => {
+  //res.send('about page');
+  res.render('about.hbs' , {
+    pageName : 'about'
+  });
+});
+
+app.listen(3000);
